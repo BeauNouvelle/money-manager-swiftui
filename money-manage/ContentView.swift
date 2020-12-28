be16daca6a7be539ed248741f893e129bbecfcb4
BeauNouvelle <beau@getswifty.dev>
2020-12-28 11:12:40 +1100

    Initial Commit

---
+++ money-manage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  money-manage
+//
+//  Created by Beau Nouvelle on 2020-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ money-manage/money_manageApp.swift
@@ -0,0 +1,17 @@
+//
+//  money_manageApp.swift
+//  money-manage
+//
+//  Created by Beau Nouvelle on 2020-12-28.
+//
+
+import SwiftUI
+
+@main
+struct money_manageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
